e0f7a8d85112190974706258061bd83d2e5e381a
hrishi-tailor <tailorhrishi@gmail.com>
2026-09-04 02:38:33 -0400

    refactor: rename base package to com.tailorcards.api

---
+++ src/main/java/com/tailorcards/api/DemoApplication.java
@@ -0,0 +1,11 @@
+package com.tailorcards.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailorCardsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TailorCardsApplication.class, args);
+    }
+}
